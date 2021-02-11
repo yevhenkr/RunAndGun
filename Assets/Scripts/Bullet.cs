27b34e86ef257f36c6fa2f85786e192927aeb173
yevhenkr <vasilenkoevgen88@gmail.com>
2021-02-11 15:15:15 +0200

    p

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,7 +7,7 @@ public class Bullet : MonoBehaviour
 
     public Rigidbody2D rb;
 
-    // Start is called before the first frame update;;
+    // Start is called before the first frame update;;kkk
     void Start()
     {
         rb.velocity = transform.right * speed;
